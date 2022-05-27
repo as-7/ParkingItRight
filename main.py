36bc102bbe117191792b2aa4c03fe82fdfce3a74
Aakash Sehrawat <39364235+as-7@users.noreply.github.com>
2022-05-27 16:11:56 +0000

    testing commit - taking input from input file

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Aakash")
+# Created by - Aakash Sehrawat
+# Date Created = 28th May 2022
+import logging
+
+
+with open('input.txt') as f:
+  lines = f.readlines()
+  print(type(lines))
+
+for input in lines:
+  input_command = input
+  print(input_command)
+
+  
